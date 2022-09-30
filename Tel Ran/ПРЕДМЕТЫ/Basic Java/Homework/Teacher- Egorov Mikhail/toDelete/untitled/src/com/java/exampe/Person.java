1ac2940ebd5851576cf0e7f3752cf2f447d49382
Семья <sema@Air-Sema.home>
2022-09-30 21:50:51 +0200

    HW2 commit

---
+++ Tel Ran/ПРЕДМЕТЫ/Basic Java/Homework/Teacher- Egorov Mikhail/toDelete/untitled/src/com/java/exampe/Person.java
@@ -0,0 +1,8 @@
+package com.java.exampe;
+
+public class Person {
+    public static void main(String[] args) {
+        String name;
+        int age;
+    }
+}
---
+++ Tel Ran/ПРЕДМЕТЫ/Basic Java/Homework/Teacher- Egorov Mikhail/toDelete/untitled/src/com/java/exampe/main.java
@@ -0,0 +1,7 @@
+package com.java.exampe;
+
+public class main {
+    public static void main(String[] args) {
+
+    }
+}
