b29ab202dc616a53e2e0684465c5d8dcb6089f43
Семья <kaloshaviktar1@gmail.com>
2022-10-05 20:44:18 +0200

    hwjava04.10

---
+++ Tel Ran/ПРЕДМЕТЫ/Basic Java/Homework/HomeWork04.10/src/main/java/com/game/hero/Elf4.java
@@ -0,0 +1,18 @@
+package com.game.hero;
+
+public class Elf4 {
+private String name_game;
+
+    public String getName_game() {
+        return name_game;
+    }
+
+    private String game;
+
+
+    public Elf4(String name_game) {
+        name_game = name_game;
+        System.out.println(name_game);
+    }
+
+}
