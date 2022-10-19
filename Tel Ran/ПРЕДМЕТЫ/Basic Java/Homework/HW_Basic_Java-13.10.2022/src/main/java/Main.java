c55250289102954ca36bdcbf9fde801cd3b4faa5
Семья <kaloshaviktar1@gmail.com>
2022-10-19 19:28:03 +0200

    lesson9

---
+++ Tel Ran/ПРЕДМЕТЫ/Basic Java/Homework/HW_Basic_Java-13.10.2022/src/main/java/Main.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+// !!!!!!!!!!!!!!!!!как решил на уроке!!!!!!!!!!!!!!!!!!!!
+
+        //Scanner day = new Scanner(System.in); //true or false
+        //System.out.println("сегодня выходной? ");
+        //Boolean isWeekend = day.nextBoolean();
+        //System.out.println("на улице дождь? ");
+        //Boolean isRain = day.nextBoolean();
+
+       // boolean result;
+
+        //if (isWeekend && isRain == false) {
+            //result = true;
+        //} else result = false;
+        //System.out.println(result);
+
+
+// !!!!!!!!!!!!!!!!! что делал я!!!!!!!!!!!!!!!!!!!!!!!
+
+        boolean isWeekend = true;
+        boolean isRain = false;
+        boolean canWalk;
+        Scanner day = new Scanner(System.in);
+        boolean name = day.nextBoolean();
+        Scanner canWalk;
+
+        if (isWeekend && isRain == true)  {
+        canWalk = true;
+            System.out.println(canWalk);
+        } else  (isWeekend && isRain == false) {
+        сanWalk = false;
+        System.out.println("Stay at home ");
+
+
+        //System.out.println("на улице дождь? ");
+       // Scanner weather = new Scanner(System.in); // true or false
+       // boolean name1 = weather.nextBoolean();
+
+        //if (isWeekend != true && isRain == false) {
+         //   canWalk = false;
+        //} else canWalk = false;
+       // System.out.println("Stay at home ");
+
+    }
+}
+// ничего не понял. опустились руки
+
+
+
+
+
