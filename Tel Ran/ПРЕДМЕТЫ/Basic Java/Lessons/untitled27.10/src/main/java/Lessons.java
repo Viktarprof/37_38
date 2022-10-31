ec1e20de50bebe54d7a13790894dc9cdee1be255
Семья <kaloshaviktar1@gmail.com>
2022-10-31 12:47:28 +0100

    lessons_12 FE

--- Tel Ran/ПРЕДМЕТЫ/Basic Java/Lessons/untitled27.10/src/main/java/Lessons.java
+++ Tel Ran/ПРЕДМЕТЫ/Basic Java/Lessons/untitled27.10/src/main/java/Lessons.java
@@ -1,5 +1,7 @@
 public class Lessons {
     public static void main(String[] args) {
+
+    }
 //        int[] intArray0;
 //        intArray0 = new int[3];
 //
@@ -15,35 +17,35 @@ public class Lessons {
 
 
     //Persson[] people = new Persson[4];
-        Persson[] people = {
+    // Persson[] people = {
 
     //1 people[0] = new Persson("Bob Marley");
         // 2
-        Persson Bob = new Persson("Bob Marley");
+     //   Persson Bob = new Persson("Bob Marley");
 
     //1 people[1] = new Persson("Lady GAGA");
         // 2
-        Persson Lady = new Persson("Lady GAGA");
+      //  Persson Lady = new Persson("Lady GAGA");
 
     //1 people[2] = new Persson("Justin Biber");
         // 2
-        Persson Justin = new Persson("Justin Biber");
+     //   Persson Justin = new Persson("Justin Biber");
 
     //1 people[3] = new Persson("Justin Timberlake");
         // 2
-        Persson Timberlake = new Persson("Justin Timberlake");
+     //   Persson Timberlake = new Persson("Justin Timberlake");
 
     //1 people[4] = new Persson("Joe Biden");
         // 2
-        Persson Joe = new Persson("Joe Biden");
+     //   Persson Joe = new Persson("Joe Biden");
 
-        for (int i = 0; i < people.length; i++) {
+        //for (int i = 0; i < people.length; i++) {
             // if (i == 0)
-            System.out.println(people[i].getFullname());
+           // System.out.println(people[i].getFullname());
             // else
             // System.out.println(people[i]); // people[i] ссылается на ечейку в памяти
-        }
-        System.out.println(people[4].getFullname());
+       // }
+       // System.out.println(people[4].getFullname());
     
 }
 }
